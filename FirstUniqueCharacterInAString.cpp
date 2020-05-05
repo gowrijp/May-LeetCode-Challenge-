c4fbfbde25813301aea950cd3c4ab254175ca635
Shreya Prasad <shreyapd06@gmail.com>
2020-05-05 17:24:12 +0530

    Update FirstUniqueCharacterInAString.cpp

--- FirstUniqueCharacterInAString.cpp
+++ FirstUniqueCharacterInAString.cpp
@@ -17,5 +17,24 @@ public:
     }
 };
 
+//Method 2
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+         unordered_map<int,int> lettersStore;
+        for(int ch=0;ch<s.length();ch++)
+        {
+            lettersStore[s[ch]]++;    //another method
+        }
+        for(int ch=0;ch<s.length();ch++)
+        {
+            if(lettersStore[s[ch]] == 1)
+                return ch;
+        }
+        return -1;  
+    }
+};
+
 //https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3320/
 // O(n)
