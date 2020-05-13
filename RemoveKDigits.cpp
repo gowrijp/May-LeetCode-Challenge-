367e19fbbdb4cc52b5477a7c6cc5abb7a5961897
Shreya Prasad <shreyapd06@gmail.com>
2020-05-14 05:21:35 +0530

    Create RemoveKDigits.cpp

---
+++ RemoveKDigits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+         string ans = "";                                         // treat ans as a stack in below for loop
+       
+       for (char c : num) {
+           while (ans.length() && ans.back() > c && k) {
+               ans.pop_back();                                  // make sure digits in ans are in ascending order
+               k--;                                             // remove one char
+           }
+           
+           if (ans.length() || c != '0') { ans.push_back(c); }  // can't have leading '0'
+       }
+       
+       while (ans.length() && k--) { ans.pop_back(); }          // make sure remove k digits in total
+       
+       return ans.empty() ? "0" : ans;
+        
+    }
+};
+
+//Time Complexity:O(k num.length())
+//LeetCode Link:https://leetcode.com/explore/featured/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3328/
