ddbff4bdc1ad7460792b1368b0abf8a123ee4166
Shreya Prasad <shreyapd06@gmail.com>
2020-06-02 09:27:51 +0530

    Update SortCharacterByFrequency.cpp

--- SortCharacterByFrequency.cpp
+++ SortCharacterByFrequency.cpp
@@ -39,3 +39,26 @@ public:
 //Runtime: 40ms
 //Memory: 8.6MB
 //LeetCode Link:https://leetcode.com/problems/sort-characters-by-frequency/
+
+//Using Priority Queue
+
+class Solution {
+public:
+    
+    string frequencySort(string s) {
+        unordered_map<char,int> freq;           //for frequency of characters
+        priority_queue<pair<int,char>> maxheap; //maxheap according to frequency of characters
+        for(char c: s)
+            freq[c]++;
+        for(auto it: freq)
+            maxheap.push({it.second,it.first}); //heap will be constructed on the basis of frequency
+        s="";   
+        while(!maxheap.empty()){
+            s+=string(maxheap.top().first,maxheap.top().second); //frequency times the character
+            maxheap.pop();
+        }
+        return s;
+    }
+   
+    
+};
