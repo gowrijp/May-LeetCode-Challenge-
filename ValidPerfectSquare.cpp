c56b1f2ea3c9909e8a283d41560cd070255947bf
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-08-25 15:43:02 +0530

    ✔️ Done (i=1; i<=num/i; i++)

--- ValidPerfectSquare.cpp
+++ ValidPerfectSquare.cpp
@@ -22,5 +22,4 @@ public:
 };
 
 //Time Complexity: O(n/i)
-//Runtime: 0ms Memory: 20mb
 //LeetCode Link: https://leetcode.com/explore/featured/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3324/
