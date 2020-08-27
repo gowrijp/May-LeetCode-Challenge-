a64e0e65f2df0a96f55438f15b61fc983800ec1e
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-08-27 12:48:35 +0530

    ✔️ total sum-min subarray sum

--- MaximumSumCircularSubarray.cpp
+++ MaximumSumCircularSubarray.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& A) {
-//         vector<int> circularArray(A.size());
-//         for(int i = 1; i<A.size(); i++){
-            
-//             circularArray[circularArray.size()-1] = A[0];
-//             circularArray[i-1] = A[i];
-//         }
         if(!A.size()){
             return 0;
         }
@@ -25,11 +19,6 @@ public:
             minTotal = min(minTotal, minSoFar);
             sum += A[i];
         }
-        // int cmax2 = circularArray[0];
-        // int max_so_far2 = circularArray[0];
-        // for(int i =1; i< circularArray.size(); i++){
-        //     cmax2 = max(circularArray[i], cmax2+circularArray[i]);
-        //     max_so_far2 = max(max_so_far2, cmax2);
         if(sum == minTotal) return max_so_far;
         return max(sum - minTotal, max_so_far);
         
