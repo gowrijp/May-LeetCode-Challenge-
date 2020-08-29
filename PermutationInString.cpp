5594a48ab4e8fb30109f7d0018dfff607528df11
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-08-29 11:05:54 +0530

    ✔️ same as anagramindices

--- PermutationInString.cpp
+++ PermutationInString.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int> map;
-        for(const char &c : s1){
-            map[c]++;
+        if(s1.length()>s2.length()) return false;
+        vector<int> mp_s2(26), mp_s1(26);
+        for(char c: s1) ++ mp_s1[c-'a'];
+        for(int i=0;i<s1.length()-1;++i){
+            ++mp_s2[s2[i]-'a'];
         }
-        int j = 0, i = 0, count = s1.size();
-        while(j < s2.size()){
-            if(map[s2.at(j++)]-- > 0){
-                count--;
-            }
-            if(count == 0){
+        for(int i=0, j=s1.length()-1;j<s2.length();++i,++j){
+            ++mp_s2[s2[j]-'a'];
+            if(mp_s1==mp_s2){
                 return true;
+                break;
             }
-            if(j - i == s1.size() && map[s2.at(i++)]++ >= 0){
-                count++;
-            }
+            --mp_s2[s2[i]-'a'];
         }
         return false;
     }
 };
 
+//https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3333/
