4e8bc297e80953cd469f4e5d51f52876db1e4771
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-08-30 10:26:15 +0530

    ✔️ Bit Manipulation Revise<>

--- CountingBits.cpp
+++ CountingBits.cpp
@@ -1,12 +1,12 @@
+//https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3343/
+
 class Solution {
 public:
-    vector<int> countBits(int num) 
-    {
+    vector<int> countBits(int num) {
         if(num < 0) return vector<int> {};
         
         vector<int> res(num + 1, 0);
-        for(int i=1;i<=num;++i)
-        {
+        for(int i=1;i<=num;++i){
             res[i] = res[i & (i - 1)] + 1;
         }
         return res;
