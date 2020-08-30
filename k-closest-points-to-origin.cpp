ae03366135e6ef68846fc50cf5282e621b4d8124
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-08-30 13:01:41 +0530

    ✔️ Done: Using a priority Queue

---
+++ k-closest-points-to-origin.cpp
@@ -0,0 +1,64 @@
+/*
+https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/538/week-5-may-29th-may-31st/3345/
+
+We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+Example 1:
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation: 
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+Example 2:
+
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+(The answer [[-2,4],[3,3]] would also be accepted.)
+
+Note:
+
+1 <= K <= points.length <= 10000
+-10000 < points[i][0] < 10000
+-10000 < points[i][1] < 10000
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        int n=points.size();
+        if(n==K)
+            return points;
+        vector<vector<int>> answer;
+        // unordered_set<vector<int>>
+        priority_queue<pair<double,vector<int>>> pq;
+        for(int i=0;i<K;i++)
+        {
+            double dist=sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
+            pq.push({dist,points[i]});
+        }
+        
+        for(int i=K;i<n;i++)
+        {
+            double dist=sqrt(points[i][0]*points[i][0]+points[i][1]*points[i][1]);
+            if(pq.top().first>dist)
+            {
+                pq.pop();
+                pq.push({dist,points[i]});
+            }
+        }
+        
+        while(!pq.empty())
+        {
+            answer.push_back(pq.top().second);
+            pq.pop();
+        }
+        return answer;
+    }
+};
