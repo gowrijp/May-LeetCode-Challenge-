56fb06b664c87e7164db95579e889b8ecda9764d
Gowri Jayaprakash <34152914+gowrijp@users.noreply.github.com>
2020-08-30 13:13:47 +0530

    ✔️ Done: DP <>

--- EditDistance.cpp
+++ EditDistance.cpp
@@ -1,3 +1,23 @@
+/*
+https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/538/week-5-may-29th-may-31st/3346/
+
+Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+*/
+
 class Solution {
 public:
     int minDistance(string word1, string word2) {
